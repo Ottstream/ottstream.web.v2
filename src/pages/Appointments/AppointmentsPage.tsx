586be2c6edb54b7b0396749c7c0deb31504fc605
anisndeyan <anisndeyan@inmood.me>
2024-03-14 18:39:51 +0400

    appointments header

--- src/pages/Appointments/AppointmentsPage.tsx
+++ src/pages/Appointments/AppointmentsPage.tsx
@@ -1,5 +1,43 @@
-const AppointmentsPage = () =>  {
-  return <div>Appointments Page</div>
-}
+import moment from 'moment';
+import { Breadcrumb } from 'antd';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+
+import { Appointments } from './Appointment.styles';
+import CustomToolbar from './Toolbar/Toolbar';
+import DayView from './Day';
+
+const localizer = momentLocalizer(moment);
+
+const AppointmentsPage = () => {
+  return (
+    <Appointments>
+      <Breadcrumb
+        className="breadcrumb"
+        items={[
+          { title: 'Home' },
+          { title: 'Appointments' },
+          { title: 'Calendar Events' },
+        ]}
+      />
+      <div className="container">
+        <Calendar
+          components={{
+            toolbar: CustomToolbar,
+          }}
+          localizer={localizer}
+          views={{
+            month: true,
+            week: true,
+            work_week: true,
+            day: DayView as never,
+          }}
+          // onDoubleClickEvent={() => alert(7)}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </div>
+    </Appointments>
+  );
+};
 
 export default AppointmentsPage;
